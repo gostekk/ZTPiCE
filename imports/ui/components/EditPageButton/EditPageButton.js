4b20a97d5f70abaac74d0e405315d5d9c2ef8851
gostekk <gostekk94@gmail.com>
2017-12-23 02:00:05 +0100

    Fix: EditPageButton now render only if authenticated

--- imports/ui/components/EditPageButton/EditPageButton.js
+++ imports/ui/components/EditPageButton/EditPageButton.js
@@ -1,19 +1,29 @@
 import React from 'react';
+import { Meteor } from 'meteor/meteor';
 import { Link } from 'react-router-dom';
+import { withTracker} from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 
-const EditPageButton = ({ pageId }) => {
-  return (
-    <Link to={`/pages/${pageId}`}>
+const EditPageButton = ({ authenticated, pageId }) => {
+  if (authenticated) {
+    return (<Link to={`/pages/${pageId}`}>
       <button>
         Edit
       </button>
-    </Link>
-  );
+    </Link>);
+  } else {
+  return null;
+  }
 }
 
 EditPageButton.propTypes = {
+  authenticated: PropTypes.bool.isRequired,
   pageId: PropTypes.string.isRequired,
 }
 
-export default EditPageButton;
+export default withTracker(() => {
+  const userId = Meteor.userId();
+  return {
+    authenticated: !!userId,
+  }
+})(EditPageButton);
