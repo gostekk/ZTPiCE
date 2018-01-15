a1c98cb693b30b90b3c7be9eca9aad9f6d0d641c
gostekk <gostekk94@gmail.com>
2018-01-15 12:29:12 +0100

    Safe users publication

--- imports/api/Users/server/publications.js
+++ imports/api/Users/server/publications.js
@@ -1,5 +1,11 @@
 import { Meteor } from 'meteor/meteor';
 
 Meteor.publish("userList", function () {
-  return Meteor.users.find({});
+  return Meteor.users.find({},
+  {
+    fields: {
+      emails: 1,
+      info: 1,
+    }
+  });
 });
