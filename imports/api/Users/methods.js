02d7d10b7e034a7690c62ec0c664965bb6f495f6
gostekk <gostekk94@gmail.com>
2018-03-25 20:07:04 +0200

    Fix2

--- imports/api/Users/methods.js
+++ imports/api/Users/methods.js
@@ -4,9 +4,9 @@ import { Accounts } from 'meteor/accounts-base';
 
 Meteor.methods({
   'user.create': function (user, roles) {
-    if (!Roles.userIsInRole(this.userId, 'admin')) {
-      throw new Meteor.Error('not-authorized', 'Must be authorized to add new user!');
-    }
+    //if (!Roles.userIsInRole(this.userId, 'admin')) {
+    //  throw new Meteor.Error('not-authorized', 'Must be authorized to add new user!');
+    //}
 
     const id = Accounts.createUser(user);
 
